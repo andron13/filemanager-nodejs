cd1a896f0d15fe793b7025492d4b8c8b270b5145
andron13 <andron13@gmail.com>
2023-06-23 20:51:06 +0200

    feat: add parameters to the decompress-function🪶

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -3,35 +3,37 @@ import zlib from "zlib";
 import {join} from "path";
 import {fileURLToPath} from "url";
 
-const inputFile = "archive.gz";
-const outputFile = "fileToCompress.txt";
+import {errorMessage} from "../constant/AppVariables.js";
 
-const folder = "files";
-const baseDir = join(fileURLToPath(import.meta.url), "..", folder);
-const inputFilePath = join(baseDir, inputFile);
-const outputFilePath = join(baseDir, outputFile);
-const errorText = "Decompression failed. Error:";
-const successText = `Decompression successful. File "${inputFile}" decompressed to "${outputFile}".`;
+export const decompress = async (inputFile, outputFile) => {
+  const baseDir = join(fileURLToPath(import.meta.url), "..");
+  const successText = `Decompression successful. File "${inputFile}" decompressed to "${outputFile}".`;
 
-const decompress = async () => {
-  const inputStream = fs.createReadStream(inputFilePath);
-  const outputStream = fs.createWriteStream(outputFilePath);
+  const inputFilePath = join(baseDir, inputFile);
+  const outputFilePath = join(baseDir, outputFile);
 
-  const unzipStream = zlib.createGunzip();
+  const readStream = fs.createReadStream(inputFilePath);
+  const writeStream = fs.createWriteStream(outputFilePath);
+  const unzipStream = zlib.createBrotliDecompress();
 
-  inputStream.pipe(unzipStream).pipe(outputStream);
+  readStream.pipe(unzipStream).pipe(writeStream);
 
   return new Promise((resolve, reject) => {
-    outputStream.on("finish", () => {
+    writeStream.on("finish", () => {
       console.log(successText);
       resolve();
     });
 
-    outputStream.on("error", (error) => {
-      console.error(errorText, error);
+    writeStream.on("error", (error) => {
+      console.error(errorMessage, error);
       reject(error);
     });
   });
 };
 
-await decompress();
+
+
+/*const inputFile = "archive.gz";
+const outputFile = "fileToCompress.txt";
+await decompress(inputFile, outputFile);
+*/
