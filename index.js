39e55ac77ee2ab3fc1a22a05733ed08c3246279e
andron13 <andron13@gmail.com>
2023-06-24 02:23:24 +0200

    feat: add switch to iterate input🪶

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+import * as os from "os";
+import * as readline from "readline";
+
 import {MESSAGES} from "./src/constant/AppVariables.js";
 
-let username = "Anonymous";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  // terminal: false
+});
 
-process.argv.map((item) => {
-  if (item.startsWith('--username=')) {
-    username = item.split('=')[1];
+/* Username start*/
+let username = "Anonymous";
+process.argv.forEach((item) => {
+  if (item.startsWith("--username=")) {
+    username = item.split("=")[1];
+  }
+});
+/* Username end*/
+
+rl.input.setEncoding("utf-8");
+process.chdir(os.homedir());
+rl.output.write(MESSAGES.welcome(username));
+rl.output.write(MESSAGES.currentPath);
+
+process.on('exit', () => {
+  process.stdout.write(MESSAGES.goodbye(username));
+});
+process.on('SIGINT', function () {
+  process.exit(0);
+});
+
+rl.on('line', async (line) => {
+  let str = line.trim();
+  console.log(str)
+  switch (str){
+    case ".exit": {
+      console.log('######### .exit ##########');
+      process.exit(0);
+      break;
+    }
+    default: {
+      console.log('######### default ##########');
+      process.stdout.write(MESSAGES.invalid)
+      break;
+    }
   }
 });
 
-process.stdout.write(MESSAGES.welcome(username) + "\n");
+
--- src/constant/AppVariables.js
+++ src/constant/AppVariables.js
@@ -1,9 +1,9 @@
 export const MESSAGES = {
-  welcome: (username) => `Welcome to the File Manager, ${username}!`,
-  goodbye: (username) => `Thank you for using File Manager, ${username}, goodbye!`,
-  currentPath: "You are currently in path_to_working_directory",
-  invalid: "Invalid input",
-  error: "Operation failed",
+  welcome: (username) => `Welcome to the File Manager, ${username}!\n`,
+  goodbye: (username) => `Thank you for using File Manager, ${username}, goodbye!\n`,
+  currentPath: `You are currently in ${process.cwd()}\n`,
+  invalid: "Invalid input\n",
+  error: "Operation failed\n",
 }
 
 
