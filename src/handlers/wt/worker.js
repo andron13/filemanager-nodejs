fb47004208a4783a5082be100133999477289e09
andron13 <andron13@gmail.com>
2023-06-26 00:17:56 +0200

    fix: minor refactoring and delete files 🪶

--- src/handlers/wt/worker.js
+++
@@ -1,9 +0,0 @@
-import {parentPort, workerData} from "node:worker_threads";
-
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-
-const sendResult = () => {
-  parentPort.postMessage(nthFibonacci(workerData));
-};
-
-sendResult();
--- src/osCommands/getEOL.js
+++ src/osCommands/getEOL.js
@@ -1,5 +1,9 @@
 import os from "node:os";
 
+/**
+ * Retrieves the default system End-Of-Line (EOL) using the `os.EOL` property
+ * and prints it to the console.
+ */
 export const getEOL = () => {
   const eol = os.EOL;
   console.log(`Default system End-Of-Line (EOL): ${JSON.stringify(eol)}`);
