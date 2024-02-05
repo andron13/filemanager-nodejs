9aa25003bc8f9fd679f2087c6460645a6ca9d7b3
andron13 <andron13@gmail.com>
2024-02-05 09:20:09 +0100

    fix: minor changes

--- src/helpers/inputInterpreter.js
+++ src/helpers/inputInterpreter.js
@@ -9,78 +9,85 @@ import { osSwitch } from '../osCommands/index.js';
  * @param {string} inputLine - Input line with the command.
  */
 export const inputChoice = async (inputLine) => {
-  const inputArray = getInputArray(inputLine);
+  try {
+    const inputArray = getInputArray(inputLine);
 
-  if (oneArgsCommands.some((el) => el === inputArray[0]) && inputArray.length > 1) {
-    aLotOfArguments(inputArray[0]);
-  }
+    if (oneArgsCommands.some((el) => el === inputArray[0]) && inputArray.length > 1) {
+      aLotOfArguments(inputArray[0]);
+      return;
+    }
 
-  if (twoArgsCommands.some((el) => el === inputArray[0]) && inputArray.length !== 2) {
-    aLotOfArguments(inputArray[0]);
-  }
-  if (threeArgsCommands.some((el) => el === inputArray[0]) && inputArray.length !== 3) {
-    aLotOfArguments(inputArray[0]);
-  }
+    if (twoArgsCommands.some((el) => el === inputArray[0]) && inputArray.length !== 2) {
+      aLotOfArguments(inputArray[0]);
+      return;
+    }
+    if (threeArgsCommands.some((el) => el === inputArray[0]) && inputArray.length !== 3) {
+      aLotOfArguments(inputArray[0]);
+      return;
+    }
 
-  switch (inputArray[0]) {
-    case 'up': {
-      goUp();
-      break;
+    switch (inputArray[0]) {
+      case 'up': {
+        goUp();
+        break;
+      }
+      case '.exit': {
+        process.exit();
+        break;
+      }
+      case 'ls': {
+        await list(process.cwd());
+        break;
+      }
+      case 'add': {
+        await create(inputArray[1]);
+        break;
+      }
+      case 'cat': {
+        await read(inputArray[1]);
+        break;
+      }
+      case 'rm': {
+        await remove(inputArray[1]);
+        break;
+      }
+      case 'cd': {
+        await cd(inputArray[1]);
+        break;
+      }
+      case 'hash':
+        await calculateHash(inputArray[1]);
+        break;
+      case 'cp': {
+        await copy(inputArray[1], inputArray[2]);
+        break;
+      }
+      case 'mv': {
+        await move(inputArray[1], inputArray[2]);
+        break;
+      }
+      case 'rn': {
+        await rename(inputArray[1], inputArray[2]);
+        break;
+      }
+      case 'decompress': {
+        decompress(inputArray[1], inputArray[2]);
+        break;
+      }
+      case 'compress': {
+        await compress(inputArray[1], inputArray[2]);
+        break;
+      }
+      case 'os':
+        osSwitch(inputArray);
+        break;
+      default: {
+        process.stdout.write(MESSAGES.invalid);
+        break;
+      }
     }
-    case '.exit': {
-      process.exit();
-      break;
-    }
-    case 'ls': {
-      await list(process.cwd());
-      break;
-    }
-    case 'add': {
-      await create(inputArray[1]);
-      break;
-    }
-    case 'cat': {
-      await read(inputArray[1]);
-      break;
-    }
-    case 'rm': {
-      await remove(inputArray[1]);
-      break;
-    }
-    case 'cd': {
-      await cd(inputArray[1]);
-      break;
-    }
-    case 'hash':
-      await calculateHash(inputArray[1]);
-      break;
-    case 'cp': {
-      await copy(inputArray[1], inputArray[2]);
-      break;
-    }
-    case 'mv': {
-      await move(inputArray[1], inputArray[2]);
-      break;
-    }
-    case 'rn': {
-      await rename(inputArray[1], inputArray[2]);
-      break;
-    }
-    case 'decompress': {
-      decompress(inputArray[1], inputArray[2]);
-      break;
-    }
-    case 'compress': {
-      await compress(inputArray[1], inputArray[2]);
-      break;
-    }
-    case 'os':
-      osSwitch(inputArray);
-      break;
-    default: {
-      process.stdout.write(MESSAGES.invalid);
-      break;
-    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
@@ -104,5 +111,4 @@ const threeArgsCommands = ['cp', 'mv', 'rn', 'decompress', 'compress'];
 const aLotOfArguments = (command) => {
   console.log('a Lot Of Arguments for: ', command);
   console.log(MESSAGES.invalid);
-  return;
 };
